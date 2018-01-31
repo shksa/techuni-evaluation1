f20f891130567989a89f4d5ba3dc5ad67d00b8e0
sreekar <sreekar339@gmail.com>
2018-01-31 14:06:13 +0530

    linter and jest configured

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+  "es6": true,
+  "jest": true,
+  "node": true
+},
+"extends": "airbnb-base",
+"rules": {
+  "semi": [
+      "error",
+      "never"
+  ],
+  "no-console": 0,
+  "no-unused-vars": 0
+}
+};
