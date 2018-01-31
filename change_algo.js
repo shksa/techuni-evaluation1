8121499567b66b697c3d20cc6844f689ac9a328f
sreekar <sreekar339@gmail.com>
2018-01-31 15:41:36 +0530

    refactored code and tested - made a seperate fn for making result list

--- change_algo.js
+++ change_algo.js
@@ -1,32 +1,39 @@
 const findAnswer = require('./ans_finder')
 
-
-function change(value, denominations) {
-  let result
-  for (let startIdx = 0; startIdx < denominations.length; startIdx += 1) {
-    result = findAnswer(startIdx, value, denominations)
-    if (result === -1) {
-      continue
-    } else {
-      break
-    }
-  }
+function makeResultList(optimalDenoms, denominations) {
   const resultList = []
-  const resSet = new Set(result)
+  const optDenomSet = new Set(optimalDenoms)
   for (let i = 0; i < denominations.length; i += 1) {
-    if (resSet.has(denominations[i])) {
+    if (optDenomSet.has(denominations[i])) {
       resultList.push(1)
     } else {
       resultList.push(0)
     }
   }
 
+  return resultList
+}
+
+
+function change(value, denominations) {
+  let optimalDenoms
+  for (let startIdx = 0; startIdx < denominations.length; startIdx += 1) {
+    optimalDenoms = findAnswer(startIdx, value, denominations)
+    if (optimalDenoms === -1) {
+      continue
+    } else {
+      break
+    }
+  }
+
+  const result = makeResultList(optimalDenoms, denominations)
+
 
   // const zerosToFill = denominations.length - result.length
   // for (let i = 0; i < zerosToFill; i += 1) {
   //   answer.push(0)
   // }
-  return resultList
+  return result
 }
 
 module.exports = { change, findAnswer }
