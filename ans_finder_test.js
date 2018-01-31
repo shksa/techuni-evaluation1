d2d07240d3a278e3e6e54a2e134ef893bdbff1cf
sreekar <sreekar339@gmail.com>
2018-01-31 15:16:19 +0530

    verified helper fn - findAnswer for i/p 0, 15, [1, 5, 10, 25, 100]

--- ans_finder_test.js
+++ ans_finder_test.js
@@ -1,7 +1,15 @@
 const findAnswer = require('./ans_finder')
 
-test('testing findAnswer fn for input 0, 15, [5, 10, 25, 100]', () => {
-  const result = findAnswer(0, 15, [5, 10, 25, 100])
-  const expected = [5, 10]
-  expect(result).toEqual(expected)
+describe('testing findAnswer fn', () => {
+  test('testing findAnswer fn for input 0, 15, [5, 10, 25, 100]', () => {
+    const result = findAnswer(0, 15, [5, 10, 25, 100])
+    const expected = [5, 10]
+    expect(result).toEqual(expected)
+  })
+
+  test('testing findAnswer fn for input 0, 15, [1, 5, 10, 25, 100]', () => {
+    const result = findAnswer(0, 15, [1, 5, 10, 25, 100])
+    const expected = -1
+    expect(result).toEqual(expected)
+  })
 })
