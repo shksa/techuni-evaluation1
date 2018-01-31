5fb94f252a3030d90852bbce32f9b415ff6b599d
sreekar <sreekar339@gmail.com>
2018-01-31 15:18:34 +0530

    verified helper fn - findAnswer for i/p 1, 15, [1, 5, 10, 25, 100]

--- ans_finder_test.js
+++ ans_finder_test.js
@@ -12,4 +12,10 @@ describe('testing findAnswer fn', () => {
     const expected = -1
     expect(result).toEqual(expected)
   })
+
+  test('testing findAnswer fn for input 1, 15, [1, 5, 10, 25, 100]', () => {
+    const result = findAnswer(1, 15, [1, 5, 10, 25, 100])
+    const expected = [5, 10]
+    expect(result).toEqual(expected)
+  })
 })
