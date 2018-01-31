a09177cdc85894963e5670dd753be6087415c9aa
sreekar <sreekar339@gmail.com>
2018-01-31 15:49:29 +0530

    verfied output - when main amt to change is equal to a denomination

--- change_test.js
+++ change_test.js
@@ -33,11 +33,11 @@ describe('when the amount to change is exactly equal to a particular denominatio
     expect(result).toEqual(expected)
   })
 
-  // test('brute force approach for input 1, [1, 5, 10, 25, 100]', () => {
-  //   const result = change(5, [1, 5, 10, 25, 100])
-  //   const expected = [0, 1, 0, 0, 0]
-  //   expect(result).toEqual(expected)
-  // })
+  test('brute force approach for input 5, [1, 5, 10, 25, 100]', () => {
+    const result = change(5, [1, 5, 10, 25, 100])
+    const expected = [0, 1, 0, 0, 0]
+    expect(result).toEqual(expected)
+  })
   // test('', () => {
   //   expect().toBe()
   // })
